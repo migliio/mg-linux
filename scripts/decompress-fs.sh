cb27b7e95ed2fa44dbb46a6590c4f3b2770a955e
Claudio Migliorelli <claudio.migliorelli@mail.polimi.it>
2023-09-08 14:28:41 +0200

    Checkpointing before moving to a self-hosted solution

--- scripts/decompress-fs.sh
+++ scripts/decompress-fs.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-mkdir initramfs
-cd initramfs
+if [! -z ./initramfs ]; then
+    read -p "The initramfs file already exists. Are you sure to delete it? [y/n] " choice
+    if [[ "${choice}" =~ ^[Yy]$ ]]; then
+	rm -rf ./initramfs
+    fi
+fi
+    
+mkdir -p initramfs
+cd ./initramfs
 cp ../initramfs.cpio.gz .
 gunzip ./initramfs.cpio.gz
 cpio -idm < ./initramfs.cpio
-rm initramfs.cpio
+rm ./initramfs.cpio

125124076379d4f5bee41a65eb800f287aee22b9
Claudio Migliorelli <claudio.migliorelli@mail.polimi.it>
2024-04-28 14:34:22 +0200

    Setting a new config file for the latest kernel with debug options

--- post-build-scripts/debug-script.sh
+++ post-build-scripts/debug-script.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 TARGETDIR=$1
 
+mkdir -p $TARGETDIR/sys/kernel/debug/ 
 echo "debugfs /sys/kernel/debug/ debugfs defaults 0 0" >> $TARGETDIR/etc/fstab
 mkdir -p $TARGETDIR/home/claudio/assets/mg-assets/kernel
 mkdir -p $TARGETDIR/root/.ssh
+rm -f $TARGETDIR/root/.ssh/authorized_keys
 cat ~/.ssh/id_ed25519.pub >> $TARGETDIR/root/.ssh/authorized_keys
 echo "hostshare /home/claudio/assets/mg-assets/kernel 9p trans=virtio,version=9p2000.L 0 0" >> $TARGETDIR/etc/fstab
